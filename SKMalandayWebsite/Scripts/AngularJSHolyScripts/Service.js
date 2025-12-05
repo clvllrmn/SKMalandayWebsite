3092a6a59d6855b84171e68ca2824a2227a784ce
Cielo Villaroman <cielo.villaroman.cics@ust.edu.ph>
2025-12-05 10:41:39 +0800

    Update Service.js

--- SKMalandayWebsite/Scripts/AngularJSHolyScripts/Service.js
+++ SKMalandayWebsite/Scripts/AngularJSHolyScripts/Service.js
@@ -1,20 +1,38 @@
-﻿app.service("SKMalandayWebsiteService", function ($http) {
-
-
-
-    this.AddUserService = function (userInfo) {
-
+app.service('SKMalandayWebsiteService', function ($http) {
+    this.AddUserService = function (user) {
         return $http({
-
-            method: "POST",
-
-            url: "/SKMalanday/VerifyConnection",
-
-            data: userInfo
-
+            method: 'POST',
+            url: '/SKMalanday/VerifyConnection',
+            data: user
         });
 
     };
+    this.GetInterestChartService = function (user) {
+        return $http.get('/SKMalanday/GetInterestChartData');
+    };
+    this.GetUserListService = function () {
+        return $http.get('/SKMalanday/GetUsers');
+    };
+
+    this.AddProjectService = function (formData) {
+        var response = $http({
+            method: "post",
+            url: '/SKMalanday/VerifyConnection',
+            data: formData,
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        });
+        return response;
+        this.uploadProject = function (formData) {
+            return $http.post('/SKMalanday/UploadProjectImage', formData, {
+                transformRequest: angular.identity,
+                headers: { 'Content-Type': undefined }
+            });
+        };
 
 
+        this.getProjects = function () {
+            return $http.get('/SKMalanday/GetProjects');
+        };
+    }
 });
